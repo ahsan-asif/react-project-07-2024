bfc4786e07e0c94c4b2686c430018fdc917feaba
Sohaib Zafer <ahsan.asif@purelogics.net>
2024-07-10 12:09:04 +0500

    Build Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return(
-    <Button colorScheme='blue'>Button</Button>
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`, // for small devices
+        lg: `"nav nav" "aside main"`, // above 1024px
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      {/* //only showing above large screen */}
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
-export default App
+export default App;
